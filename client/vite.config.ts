6d5c3082d01c1b85b26f921c435be7ab8ef5bd85
jz <jahanzaibkhan.te@gmail.com>
2025-08-18 20:14:46 +0500

    new build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // No plugin needed for basic React 18 JSX, but keeping placeholder in case
 export default defineConfig({
+	base: '/custom-chatbot/',
 	server: {
 		port: 13241,
 		host: true
+	},
+	build: {
+		assetsDir: 'assests'
 	},
 	plugins: [react()]
 })
